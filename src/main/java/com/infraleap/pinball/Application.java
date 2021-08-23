0c49c6235a8d85354e0da19733d478303af3d6a0
Enver Haase <enver@vaadin.com>
2021-08-23 20:28:30 +0200

    initial commit

---
+++ src/main/java/com/infraleap/pinball/Application.java
@@ -0,0 +1,22 @@
+package com.infraleap.pinball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import org.vaadin.artur.helpers.LaunchUtil;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/permanent-marker", version = "4.5.0")
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+    }
+
+}
