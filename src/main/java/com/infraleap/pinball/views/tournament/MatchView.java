f2efc5d6ca8e27bd1753f041c5b872fe49ae4344
Enver Haase <enver@vaadin.com>
2021-09-28 00:21:17 +0200

    rough first impl of the match view.

--- src/main/java/com/infraleap/pinball/views/tournament/MatchView.java
+++ src/main/java/com/infraleap/pinball/views/tournament/MatchView.java
@@ -1,19 +1,22 @@
 package com.infraleap.pinball.views.tournament;
 
+import ch.qos.logback.core.Layout;
 import com.infraleap.pinball.components.VerticalAutoScroller;
 import com.infraleap.pinball.data.matchplay.*;
 import com.infraleap.pinball.layout.MainLayout;
 import com.infraleap.pinball.service.MatchPlayService;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.BeforeEvent;
-import com.vaadin.flow.router.HasUrlParameter;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.*;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 @PageTitle("Standings")
 @Route(value = "match", layout = MainLayout.class)
@@ -24,6 +27,7 @@ public class MatchView extends VerticalLayout implements HasUrlParameter<String>
     private Tournament tournament;
     private Standing[] standings;
     private Result[] results;
+    private String round_id = null;
 
     public MatchView(MatchPlayService matchPlayService) {
         this.matchPlayService = matchPlayService;
@@ -57,15 +61,62 @@ public class MatchView extends VerticalLayout implements HasUrlParameter<String>
             vl.getElement().getStyle().set("top", "0");
         }
 
+        VerticalAutoScroller vas = new VerticalAutoScroller();
+        add(vas);
+
+
         if (results != null){
-            for (Result result : results){
-                for (Game game : result.getGames()){
+            // TODO: sort by starting UTC time, do not trust server order
+            Result round = null;
+            if (this.round_id == null){
+                round = results[results.length-1];
+            }
+            else{
+                for (Result result : results){
+                    if (result.getRoundId().equals(Integer.parseInt(round_id))){
+                        round = result;
+                        break;
+                    }
+                }
+            }
+
+
+            if (round != null){
+                for (Game game : round.getGames()) {
+                    // Game -- arena with players
+                    Integer arenaId = game.getArenaId();
+                    Arena arena = arenaId == null ? null : matchPlayService.getArenaWithId(tournament.getTournamentId(), arenaId);
+                    String arenaHeader = ("Arena: " + (arena == null ? "(none)" : arena.getName()));
+
+                    VerticalLayout gameLayout = new VerticalLayout();
+                    gameLayout.addClassName("bordered");
+                    gameLayout.add(new H3(arenaHeader));
+                    VerticalLayout playersLayout = new VerticalLayout();
+                    playersLayout.setWidthFull();
+                    gameLayout.add(playersLayout);
+
+                    Result[] results = game.getResults().toArray(new Result[0]);
+                    //Arrays.sort(results, Comparator.comparingDouble(result -> Double.parseDouble((String) result.getAdditionalProperties().get("points"))));
 
                     Player[] players = game.getPlayers().toArray(new Player[0]);
-                    //Arrays.sort(players, Comparator.comparingInt( player -> player.getGame() ));
-                    for (Player player : players){
-                        System.out.println(player);
+                    Arrays.sort(players, Comparator.comparingInt(player -> (int) player.getGame().getAdditionalProperties().get("index")));
+
+                    for (Player player : players) {
+                        String points = " : ";
+                        for (Result result : results){
+                            if (result.getAdditionalProperties() != null) {
+                                if (player.getPlayerId().equals(result.getAdditionalProperties().get("player_id"))) {
+                                    points += result.getAdditionalProperties().get("points");
+                                    points += "pts";
+                                    break;
+                                }
+                            }
+                        }
+
+                        playersLayout.add(new H2(player.getName()+points));
                     }
+
+                    vas.add(gameLayout);
                 }
             }
 
@@ -75,37 +126,32 @@ public class MatchView extends VerticalLayout implements HasUrlParameter<String>
 
 
 
-            VerticalAutoScroller vas = new VerticalAutoScroller();
-            add(vas);
-
-            for (Standing standing : this.standings){
-                VerticalLayout verticalLayout = new VerticalLayout();
-                verticalLayout.addClassName("bordered");
-
-                HorizontalLayout standingLayout = new HorizontalLayout();
-                standingLayout.setWidthFull();
-                standingLayout.add(new H3(standing.getPosition().toString()+": "+standing.getName()));
-                FlexLayout wrapper = new FlexLayout(new H4("Points: "+standing.getPoints()));
-                wrapper.setJustifyContentMode(JustifyContentMode.END);
-                standingLayout.add(wrapper);
-                standingLayout.expand(wrapper);
-
-                standingLayout.setAlignItems(Alignment.BASELINE);
 
 
-                verticalLayout.add(standingLayout);
-                vas.add(verticalLayout);
-            }
 
         }
 
     }
 
     @Override
-    public void setParameter(BeforeEvent beforeEvent, String s) {
-        tournament = matchPlayService.getTournament(s);
-        standings = matchPlayService.getStandings(s);
-        results = matchPlayService.getResults(s);
+    public void setParameter(BeforeEvent beforeEvent, @WildcardParameter String s) {
+
+        StringTokenizer st = new StringTokenizer(s, "/");
+
+        String tourney = null;
+
+        if (st.hasMoreTokens()){
+            tourney = st.nextToken();
+        }
+        if (st.hasMoreTokens()){
+            round_id = st.nextToken();
+        }
+
+        if (tourney != null) {
+            tournament = matchPlayService.getTournament(tourney);
+            standings = matchPlayService.getStandings(tourney);
+            results = matchPlayService.getResults(tourney);
+        }
 
         update();
     }
