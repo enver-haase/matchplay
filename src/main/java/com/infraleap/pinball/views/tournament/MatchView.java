5d789e5e65f09a3978366a05c84551d62716c1c0
Enver Haase <enver@vaadin.com>
2021-09-28 08:10:28 +0200

    Add a round number.

--- src/main/java/com/infraleap/pinball/views/tournament/MatchView.java
+++ src/main/java/com/infraleap/pinball/views/tournament/MatchView.java
@@ -23,7 +23,6 @@ public class MatchView extends VerticalLayout implements HasUrlParameter<String>
     private final MatchPlayService matchPlayService;
 
     private Tournament tournament;
-    private Standing[] standings;
     private Result[] results;
     private String round_id = null;
 
@@ -47,86 +46,83 @@ public class MatchView extends VerticalLayout implements HasUrlParameter<String>
 
         removeAll();
 
-        if (tournament != null) {
-            VerticalLayout vl = new VerticalLayout();
-            HorizontalLayout hl = new HorizontalLayout(new H1(tournament.getName()));
-            hl.setJustifyContentMode(JustifyContentMode.CENTER);
-            hl.setWidthFull();
-            vl.add(new H4("Welcome To: "), hl);
-            vl.getStyle().set("background", "#33cccc");
-            add(vl);
-            vl.getElement().getStyle().set("position", "sticky");
-            vl.getElement().getStyle().set("top", "0");
-        }
-
-        VerticalAutoScroller vas = new VerticalAutoScroller();
-        add(vas);
-
-
-        if (results != null){
+        Result round = null;
+        int roundNo = 0;
+        if (results != null) {
             // TODO: sort by starting UTC time, do not trust server order
-            Result round = null;
-            if (this.round_id == null){
-                round = results[results.length-1];
-            }
-            else{
-                for (Result result : results){
-                    if (result.getRoundId().equals(Integer.parseInt(round_id))){
+
+            if (this.round_id == null) {
+                round = results[results.length - 1];
+                roundNo = results.length;
+            } else {
+                for (int i=0; i<results.length; i++) {
+                    Result result = results[i];
+                    if (result.getRoundId().equals(Integer.parseInt(round_id))) {
                         round = result;
+                        roundNo = i+1;
                         break;
                     }
                 }
             }
+        }
 
 
-            if (round != null){
-                for (Game game : round.getGames()) {
-                    // Game -- arena with players
-                    Integer arenaId = game.getArenaId();
-                    Arena arena = arenaId == null ? null : matchPlayService.getArenaWithId(tournament.getTournamentId(), arenaId);
-                    String arenaHeader = ("Arena: " + (arena == null ? "(none)" : arena.getName()));
+        if (tournament != null) {
+            VerticalLayout vl = new VerticalLayout();
+            HorizontalLayout tourneyNameLayout = new HorizontalLayout(new H1(tournament.getName()));
+            tourneyNameLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+            tourneyNameLayout.setWidthFull();
+            HorizontalLayout roundLayout = new HorizontalLayout(new H2("Round "+roundNo));
+            roundLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+            roundLayout.setWidthFull();
+            vl.add(new H4("Welcome To: "), tourneyNameLayout, roundLayout);
+            vl.getStyle().set("background", "#33cccc");
+            vl.getElement().getStyle().set("position", "sticky");
+            vl.getElement().getStyle().set("top", "0");
+            add(vl);
+        }
 
-                    VerticalLayout gameLayout = new VerticalLayout();
-                    gameLayout.addClassName("bordered");
-                    gameLayout.add(new H3(arenaHeader));
-                    VerticalLayout playersLayout = new VerticalLayout();
-                    playersLayout.setWidthFull();
-                    gameLayout.add(playersLayout);
 
-                    Result[] results = game.getResults().toArray(new Result[0]);
-                    //Arrays.sort(results, Comparator.comparingDouble(result -> Double.parseDouble((String) result.getAdditionalProperties().get("points"))));
+        VerticalAutoScroller vas = new VerticalAutoScroller();
+        add(vas);
 
-                    Player[] players = game.getPlayers().toArray(new Player[0]);
-                    Arrays.sort(players, Comparator.comparingInt(player -> (int) player.getGame().getAdditionalProperties().get("index")));
+        if (round != null){
+            for (Game game : round.getGames()) {
+                // Game -- arena with players
+                Integer arenaId = game.getArenaId();
+                Arena arena = arenaId == null ? null : matchPlayService.getArenaWithId(tournament.getTournamentId(), arenaId);
+                String arenaHeader = ("Arena: " + (arena == null ? "(none)" : arena.getName()));
 
-                    for (Player player : players) {
-                        String points = " : ";
-                        for (Result result : results){
-                            if (result.getAdditionalProperties() != null) {
-                                if (player.getPlayerId().equals(result.getAdditionalProperties().get("player_id"))) {
-                                    points += result.getAdditionalProperties().get("points");
-                                    points += "pts";
-                                    break;
-                                }
+                VerticalLayout gameLayout = new VerticalLayout();
+                gameLayout.addClassName("bordered");
+                gameLayout.add(new H3(arenaHeader));
+                VerticalLayout playersLayout = new VerticalLayout();
+                playersLayout.setWidthFull();
+                gameLayout.add(playersLayout);
+
+                Result[] results = game.getResults().toArray(new Result[0]);
+                //Arrays.sort(results, Comparator.comparingDouble(result -> Double.parseDouble((String) result.getAdditionalProperties().get("points"))));
+
+                Player[] players = game.getPlayers().toArray(new Player[0]);
+                Arrays.sort(players, Comparator.comparingInt(player -> (int) player.getGame().getAdditionalProperties().get("index")));
+
+                for (Player player : players) {
+                    String points = "\u00a0: "; // non-breaking space before the colon
+                    for (Result result : results){
+                        if (result.getAdditionalProperties() != null) {
+                            if (player.getPlayerId().equals(result.getAdditionalProperties().get("player_id"))) {
+                                points += result.getAdditionalProperties().get("points");
+                                points += "pts";
+                                break;
                             }
                         }
-
-                        playersLayout.add(new H2(player.getName()+points));
                     }
 
-                    vas.add(gameLayout);
+                    playersLayout.add(new H2(player.getName()+points));
                 }
+
+                vas.add(gameLayout);
             }
-
-
-
-
-
-
-
-
-
-
         }
 
     }
@@ -144,7 +140,6 @@ public class MatchView extends VerticalLayout implements HasUrlParameter<String>
 
         if (tourney != null) {
             tournament = matchPlayService.getTournament(tourney);
-            standings = matchPlayService.getStandings(tourney);
             results = matchPlayService.getResults(tourney);
         }
 
