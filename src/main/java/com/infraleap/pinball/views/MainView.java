d25542742dbf90e7b888419f988e8f48ceffce82
Enver Haase (vaadin) <enver@vaadin.com>
2024-05-18 22:48:01 +0200

    click listener.

--- src/main/java/com/infraleap/pinball/views/MainView.java
+++ src/main/java/com/infraleap/pinball/views/MainView.java
@@ -1,6 +1,7 @@
 package com.infraleap.pinball.views;
 
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
 
 @Route("")
@@ -9,5 +10,7 @@ public class MainView extends Image {
 		super("images/flipperstudio.png", "Flipperstudio cameras");
 		setWidth("100%");
 		setHeight("100%");
+
+		addClickListener( e -> Notification.show("Nothing here yet. Tournament history and score-tracking coming soon."));
 	}
 }
